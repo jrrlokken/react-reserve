c2221cf75e2818b95bf2cdf6a0a00a7c1b641c2c
Joshua Lokken <jrrlokken@gmail.com>
2019-10-13 09:54:31 -0500

    Implemented Shopping Cart page with styles

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,18 @@
+import { Button, Segment, Divider } from 'semantic-ui-react';
+
 function CartSummary() {
-  return <>CartSummary</>;
+  return <>
+    <Divider />
+    <Segment clearing size="large">
+      <strong>Subtotal:</strong> $0.00
+      <Button
+        icon="cart"
+        color="teal"
+        floated="right"
+        content="Checkout"
+      />
+    </Segment>
+  </>;
 }
 
 export default CartSummary;
