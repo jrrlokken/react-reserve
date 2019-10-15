1de888e878ec9a8bfd3b5442af73af0c26ce6b78
Joshua Lokken <jrrlokken@gmail.com>
2019-10-14 19:05:04 -0500

    Implemented full cart and checkout functionality, including Stripe integration

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,7 +1,7 @@
-import { Header, Segment, Button, Icon, Item } from 'semantic-ui-react';
+import { Header, Segment, Button, Icon, Item, Message } from 'semantic-ui-react';
 import { useRouter } from 'next/router';
 
-function CartItemList({ products, user }) {
+function CartItemList({ products, user, handleRemoveFromCart, success }) {
   const router = useRouter();
 
   function mapCartProductsToItems(products) {
@@ -20,12 +20,22 @@ function CartItemList({ products, user }) {
           basic
           icon="remove"
           floated="right"
-          onClick={() => console.log(p.product._id)}
+          onClick={() => handleRemoveFromCart(p.product._id)}
         />
       )
     }))
   }
 
+  if (success) {
+    return (
+      <Message 
+        success
+        header="Success!"
+        content="Your order and payment has been accepted"
+        icon="star outline"
+      />
+    )
+  }
   if (products.length === 0) {
     return (
       <Segment secondary color="teal" inverted textAlign="center" placeholder>
@@ -52,7 +62,7 @@ function CartItemList({ products, user }) {
     );
   }
 
-  return <Item.Group items={mapCartProductsToItems(products)} />
+  return <Item.Group divided items={mapCartProductsToItems(products)} />
 }
 
 export default CartItemList;
--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,16 +1,44 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 import { Button, Segment, Divider } from 'semantic-ui-react';
+import calculateCartTotal from '../../utils/calculateCartTotal';
 
-function CartSummary() {
+function CartSummary({ products, handleCheckout, success }) {
+  const [cartAmount, setCartAmount] = React.useState(0);
+  const [stripeAmount, setStripeAmount] = React.useState(0);
+  const [isCartEmpty, setCartEmpty] = React.useState(false);
+
+  React.useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
+    setCartEmpty(products.length === 0);
+  }, [products]);
+
   return <>
     <Divider />
     <Segment clearing size="large">
-      <strong>Subtotal:</strong> $0.00
+      <strong>Subtotal:</strong> ${cartAmount}
+      <StripeCheckout
+        name="Racing Rabbit"
+        amount={stripeAmount}
+        image={products.length > 0 ? products[0].product.mediaUrl : "" }
+        currency="USD"
+        shippingAddress={true}
+        billingAddress={true}
+        zipCode={true}
+        stripeKey="pk_test_CDHFOoSp79lNHuKvUqYXD3pJ006bbkLu2d"
+        token={handleCheckout}
+        triggerEvent="onClick"
+      >
       <Button
         icon="cart"
         color="teal"
         floated="right"
         content="Checkout"
+        disabled={isCartEmpty || success}
       />
+      </StripeCheckout>
     </Segment>
   </>;
 }
