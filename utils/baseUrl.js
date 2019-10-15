65179b0f24c717916a911f359dccb69b480b67d7
Joshua Lokken <jrrlokken@gmail.com>
2019-10-15 16:53:40 -0500

    Update package.json

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,7 +1,7 @@
-const baseUrl = 'https://racing-rabbit.herokuapp.com';
+const baseUrl = process.env.NODE_ENV === "production"
+  ? 'https://racing-rabbit.herokuapp.com'
+  : 'http://localhost:3000';
 
-// const baseUrl = process.env.NODE_ENV === "production"
-//   ? 'https://racing-rabbit.herokuapp.com'
-//   : 'http://localhost:3000'
+// const baseUrl = 'https://racing-rabbit.herokuapp.com';
 
   export default baseUrl;
